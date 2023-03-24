47f4e0db622e1bc1ce12c5d9a73144de116a5319
Thepope-amenos <alae.baboudi1@gmail.com>
2023-03-23 19:49:47 -0700

    commited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,23 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf("Fizz\n");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf("Buzz\n");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz\n");
 		}
 		else if (i == 1)
 		{
-			printf("%d", i);
+			printf("%d\n", i);
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d\n", i);
 		}
 	}
 	printf("\n");
